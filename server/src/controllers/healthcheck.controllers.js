528dd3dab1016cfba4fb702c21e4e1e27a020cba
Soumaditya <soumadityadey9933@gmail.com>
2025-09-14 18:04:17 +0530

    error while taking the api response and api error , fixed the utils typo

--- server/src/controllers/healthcheck.controllers.js
+++ server/src/controllers/healthcheck.controllers.js
@@ -1,5 +1,5 @@
-import {ApiResponse} from '../Utils/ApiResponse.js';
-import {asyncHandler} from "../Utils/asyncHandler.js";
+import {ApiResponse} from '../utils/ApiResponse.js';
+import {asyncHandler} from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler( async (req, res) => {
     return res.status(200).json(new ApiResponse(200,"OK","Health Check Passed",));
